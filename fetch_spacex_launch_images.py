0cb9308b6f9607d48e6bd1a2f5f9690bd2da6d66
Valery Averyanov <109584765+VAChess777@users.noreply.github.com>
2022-10-21 18:17:35 +0300

    Update fetch_spacex_launch_images.py

--- fetch_spacex_launch_images.py
+++ fetch_spacex_launch_images.py
@@ -3,26 +3,26 @@ from pathlib import Path
 import requests
 
 
-def picture_latest_launch(path, url):
+def get_pictures_latest_launch(path, url):
     url_latest_launch = f"{url}/{'latest'}"
     response = requests.get(url_latest_launch)
     response.raise_for_status()
     image_latest_launch = response.json()["links"]["flickr"]["original"]
-    for images_number, image_latest_launch in enumerate(image_latest_launch, 1):
-        filename = f'spacex_latest_launch_{images_number}.jpg'
+    for images_numbers, image_latest_launch in enumerate(image_latest_launch, 1):
+        filename = f'spacex_latest_launch_{images_numbers}.jpg'
         image = requests.get(image_latest_launch).content
-        with open(f'{path}/{filename}', 'wb') as file:
+        with open(Path(f'{path}', f'{filename}'), 'wb') as file:
             file.write(image)
 
 
-def spacex_id_launch_picture(spacex_launch_id, path, url):
+def get_pictures_by_launch_id(spacex_launch_id, path, url):
     response = requests.get(url)
     response.raise_for_status()
     images_launch = response.json()[spacex_launch_id]['links']['flickr']['original']
-    for images_number, images_launch in enumerate(images_launch, 1):
-        filename = f'spacex_{images_number}.jpg'
+    for images_numbers, images_launch in enumerate(images_launch, 1):
+        filename = f'spacex_{images_numbers}.jpg'
         image = requests.get(images_launch).content
-        with open(f'{path}/{filename}', 'wb') as file:
+        with open(Path(f'{path}', f'{filename}'), 'wb') as file:
             file.write(image)
 
 
@@ -50,10 +50,10 @@ def main():
     parser = create_parser()
     spacex_launch_id = parser.parse_args().id
     if spacex_launch_id:
-        spacex_id_launch_picture(spacex_launch_id, path, url)
+        get_pictures_by_launch_id(spacex_launch_id, path, url)
     else:
-        picture_latest_launch(path, url)
-        if picture_latest_launch(path, url) is None:
+        get_pictures_latest_launch(path, url)
+        if get_pictures_latest_launch(path, url) is None:
             print('There are no images from the last launch spacex on the server')
 
 
